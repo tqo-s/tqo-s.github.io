950c27b0d93f41a5380b1cc1a16754738656a6ee
tqo-s <68671802+tqo-s@users.noreply.github.com>
2020-07-29 14:11:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+{
+    var btn = document.getElementById('omikuji-btn');
+    var img = document.getElementById('omikuji-img');
+
+    btn.addEventListener('click',function(){
+        var n = Math.floor(Math.random()*3);
+        //btn.textContent=n;
+        switch(n){
+            case 0:
+                btn.textContent = '大吉';
+                img.src = "../images/daikichi.png";
+                break;
+            case 1:
+                btn.textContent = '中吉';
+                img.src = "../images/chukichi.png";
+                break;
+            case 2:
+                btn.textContent = '吉';
+                img.src = "../images/kichi.png";
+                break;
+        }
+    });
+}
